73bb6c29779a4592972481fa50db55815f2e0e09
Santosl2c <mfilype2019@gmail.com>
2022-06-21 22:17:05 -0300

    feat: implements formSubmit and input state

--- src/components/Form/Button/Button.types.ts
+++ src/components/Form/Button/Button.types.ts
@@ -3,5 +3,5 @@ import { HTMLAttributes, ReactNode } from "react";
 export type ButtonProps = HTMLAttributes<HTMLButtonElement> & {
   children: ReactNode;
   type: "button" | "submit" | "reset" | undefined;
-  disabled: boolean;
+  disabled?: boolean;
 };
--- src/components/Form/MainForm/MainForm.tsx
+++ src/components/Form/MainForm/MainForm.tsx
@@ -1,13 +1,37 @@
+import { FormEvent, useCallback, useMemo, useRef, useState } from "react";
+
 import { PlusCircle } from "phosphor-react";
 
 import { Button } from "../Button";
 import { Input } from "../Input";
 
 export function MainForm(): JSX.Element {
+  const [inputValue, setInputValue] = useState("");
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const buttonIsDisabled = useMemo(
+    () => inputValue.trim().length <= 0,
+    [inputValue]
+  );
+
+  const handleSubmit = useCallback((e: FormEvent) => {
+    e.preventDefault();
+    console.log("hello world");
+  }, []);
+
   return (
-    <form className="flex w-full items-center justify-center gap-2">
-      <Input placeholder="O que você precisa fazer?" />
-      <Button type="submit" disabled>
+    <form
+      className="flex w-full items-center justify-center gap-2"
+      ref={formRef}
+      onSubmit={handleSubmit}
+    >
+      <Input
+        placeholder="O que você precisa fazer?"
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setInputValue(e.target.value);
+        }}
+      />
+      <Button type="submit" disabled={buttonIsDisabled}>
         Criar <PlusCircle weight="bold" size={16} />
       </Button>
     </form>
