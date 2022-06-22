cfe48ac3602df3f8693e3e7cdb5609bccc43b378
Santosl2c <mfilype2019@gmail.com>
2022-06-22 10:32:24 -0300

    feat: implemented redux and redux persist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,20 @@
 import type { AppProps } from "next/app";
 
 import "../styles/globals.scss";
+import { Provider } from "react-redux";
+
+import { PersistGate } from "redux-persist/integration/react";
+
+import { persistor, store } from "@/shared/store";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={<>Loading</>} persistor={persistor}>
+        <Component {...pageProps} />;
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default MyApp;
---
+++ src/shared/store/index.ts
@@ -0,0 +1,36 @@
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+import { combinedReducer } from "./reducers";
+
+const persistConfig = {
+  key: "todos",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, combinedReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store, {}, () => {
+  persistor.persist();
+});
