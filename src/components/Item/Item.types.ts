816f6b54fd1412c8587d296ea816176b12c1f9f0
Santosl2c <mfilype2019@gmail.com>
2022-06-22 09:21:32 -0300

    feat: Todo Item created

---
+++ src/components/Item/Item.types.ts
@@ -0,0 +1,4 @@
+export type ItemProps = {
+  id: number;
+  text: string;
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./Header";
 export * from "./Form";
 export * from "./Badge";
 export * from "./TextWithBadge";
+export * from "./Item";
