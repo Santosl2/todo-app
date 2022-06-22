8827f97e8ef0a4baa57911ddad435dadbefba4c8
Santosl2c <mfilype2019@gmail.com>
2022-06-22 10:02:23 -0300

    feat: added Item animation

--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,14 +1,33 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import { useCallback, useMemo, useState } from "react";
 
+import { motion, AnimatePresence, Variants } from "framer-motion";
 import { Trash } from "phosphor-react";
 
 import { ItemProps } from "./Item.types";
 
+const itemVariants: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 0.4,
+    },
+  },
+  exit: {
+    opacity: 0,
+  },
+};
+
 export function Item({ id, text }: ItemProps) {
   const [checkBoxChecked, setCheckBoxChecked] = useState(false);
+  const [isVisble, setIsVisble] = useState(true);
 
   const handleDelete = useCallback(() => {
+    setIsVisble(false);
+
     console.log("delete", id);
   }, []);
 
@@ -23,23 +42,34 @@ export function Item({ id, text }: ItemProps) {
   }, [checkBoxChecked]);
 
   return (
-    <div className="mb-3 flex w-full justify-between rounded-lg bg-gray-500 p-4">
-      <label className="flex items-center gap-3">
-        <input
-          id="checkbox"
-          type="checkbox"
-          onChange={(e) => setCheckBoxChecked(e.target.checked)}
-          className="h-4 w-4 cursor-pointer rounded-full border-blue-300 bg-transparent checked:bg-purple-500 checked:hover:bg-purple-500 focus:ring-0 transition-all"
-        />
-        <p className={classNamesFromText}>{text}</p>
-      </label>
-      <button
-        className="text-gray-300 transition-colors hover:text-red-500 hover:bg-gray-600 p-1 rounded"
-        type="button"
-        onClick={handleDelete}
-      >
-        <Trash size={24} />
-      </button>
-    </div>
+    <AnimatePresence>
+      {isVisble && (
+        <motion.li
+          layout
+          variants={itemVariants}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          className="mb-3 flex w-full justify-between rounded-lg bg-gray-500 p-4"
+        >
+          <label className="flex items-center gap-3">
+            <input
+              id="checkbox"
+              type="checkbox"
+              onChange={(e) => setCheckBoxChecked(e.target.checked)}
+              className="h-4 w-4 cursor-pointer rounded-full border-blue-300 bg-transparent checked:bg-purple-500 checked:hover:bg-purple-500 focus:ring-0 transition-all"
+            />
+            <p className={classNamesFromText}>{text}</p>
+          </label>
+          <button
+            className="text-gray-300 transition-colors hover:text-red-500 hover:bg-gray-600 p-1 rounded"
+            type="button"
+            onClick={handleDelete}
+          >
+            <Trash size={24} />
+          </button>
+        </motion.li>
+      )}
+    </AnimatePresence>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
+import { motion, LayoutGroup, Variants } from "framer-motion";
+
 import { Item, MainForm, TextWithBadge } from "@/components";
 import { Header } from "@/components/Header";
 import { SEO } from "@/SEO";
 
+const listVariants: Variants = {
+  visible: {
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+  hidden: {},
+};
 export default function Home() {
   return (
     <>
@@ -15,11 +25,20 @@ export default function Home() {
             <TextWithBadge value="0" text="Tarefas criadas" />
             <TextWithBadge value="0 de 0" text="Tarefas concluídas" />
           </div>
-          <Item id={7} text="Tarefas" />
-          <Item id={7} text="Tarefas" />
-          <Item id={7} text="Tarefas" />
-          <Item id={7} text="Tarefas" />
-          <Item id={7} text="Tarefas" />
+          <LayoutGroup>
+            <motion.ul
+              variants={listVariants}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+            >
+              <Item id={7} text="Tarefas" />
+              <Item id={7} text="Tarefas" />
+              <Item id={7} text="Tarefas" />
+              <Item id={7} text="Tarefas" />
+              <Item id={7} text="Tarefas" />
+            </motion.ul>
+          </LayoutGroup>
         </div>
       </main>
     </>
