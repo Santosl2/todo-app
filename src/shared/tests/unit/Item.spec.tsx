15a8a22d2579828ed22917d3ae09230e73b85495
Santosl2c <mfilype2019@gmail.com>
2022-06-23 21:28:47 -0300

    tests: Item.tsx unit tests

--- src/shared/tests/unit/Item.spec.tsx
+++ src/shared/tests/unit/Item.spec.tsx
@@ -1,74 +1,82 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable unused-imports/no-unused-imports */
 
-import * as ReactRedux from "react-redux";
+import { act } from "react-dom/test-utils";
 
 import { Item } from "@/components";
-import { Todo } from "@/shared/interfaces/Todo";
 import { fireEvent, screen } from "@testing-library/react";
 
-import { customRender } from "../utils/CustomRender";
+import { renderWithRedux } from "../utils/CustomRender";
 
-jest.mock("react-redux", () => ({
-  ...jest.requireActual("react-redux"),
-  useSelector: jest.fn(),
-  useDispatch: jest.fn(),
-}));
+// jest.mock("react-redux", () => ({
+//   ...jest.requireActual("react-redux"),
+//   useSelector: jest.fn(),
+//   useDispatch: jest.fn(),
+// }));
 
-const mockedSelectorTodos = jest.spyOn(
-  require("../../hooks/useSelectorTodos"),
-  "useSelectorTodos"
-);
+// const mockedSelectorTodos = jest.spyOn(
+//   require("../../hooks/useSelectorTodos"),
+//   "useSelectorTodos"
+// );
+
+const initialState = {
+  todos: [
+    {
+      id: 1,
+      value: "Test Task",
+      isCompleted: false,
+    },
+  ],
+};
+
+function render() {
+  const { mockStore, rerenderWithRedux } = renderWithRedux(
+    <Item id={1} text="Test Task" />,
+    initialState
+  );
+
+  return { mockStore, rerenderWithRedux };
+}
 
 describe("Item", () => {
-  let mockArray: Todo[] = [];
-  const useDispatchMock = ReactRedux.useDispatch;
-
-  beforeEach(() => {
-    mockedSelectorTodos.mockImplementation(() => {
-      return mockArray;
-    });
-  });
-
-  afterEach(() => {
-    mockArray = [];
-    mockedSelectorTodos.mockClear();
-    (useDispatchMock as jest.Mock).mockClear();
-  });
-
   it("should render correctly", () => {
-    customRender(<Item id={0} text="Test Task" />);
+    render();
 
     expect(screen.getByText("Test Task")).toBeInTheDocument();
   });
 
-  it("should be able to change Todo to completed", () => {
-    (useDispatchMock as jest.Mock).mockImplementation(() => {
-      mockArray.push({
-        id: 1,
-        isCompleted: true,
-        value: "Test Task Snap",
-      });
+  it("should be able to change Todo to completed", async () => {
+    act(() => {
+      render();
     });
-
-    customRender(<Item id={1} text="Test Task Snap" />);
 
     const todoCheckBox = screen.getByTestId(
       "checkboxItemTest"
     ) as HTMLInputElement;
 
-    fireEvent.change(todoCheckBox, { target: { checked: true } });
+    fireEvent.click(todoCheckBox);
 
-    expect(useDispatchMock).toHaveBeenCalled();
-    expect(todoCheckBox.checked).toBe(true);
-    expect(screen.getByText("Test Task Snap")).toHaveClass("line-through");
-    expect(screen.getByText("Test Task Snap")).toHaveClass("text-gray-300");
+    expect(screen.getByText("Test Task")).toHaveClass("line-through");
+
+    expect(screen.getByText("Test Task")).toHaveClass("text-gray-300");
+  });
+
+  it("should be able to delete Todo", async () => {
+    act(() => {
+      render();
+    });
+
+    const deleteBtn = screen.getByTestId("deleteItemTest") as HTMLInputElement;
+
+    fireEvent.click(deleteBtn);
+
+    expect(screen.queryByText("Test Task")).not.toBeInTheDocument();
   });
 
   it("match snapshot", () => {
-    customRender(<Item id={2} text="Test Task Snap" />);
-
-    expect(screen.getByText("Test Task Snap")).toMatchSnapshot();
+    render();
+    expect(screen.getByText("Test Task")).toMatchSnapshot();
   });
 });
